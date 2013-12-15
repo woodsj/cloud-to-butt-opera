c5f4f6bd825375c1d45aa3b0c51ab98935d631ec
woodsj <jowo@thionite.net>
2013-12-15 13:03:48 -0600

    Update cloud-to-butt.js
    XKCD+ Edition. The truth will set you free. Either that, or I'm goofing off on a Sunday afternoon.

--- includes/cloud-to-butt.js
+++ includes/cloud-to-butt.js
@@ -35,6 +35,38 @@
         v = v.replace(/\bThe cloud\b/g, "My butt");
         v = v.replace(/\bthe Cloud\b/g, "my Butt");
         v = v.replace(/\bthe cloud\b/g, "my butt");
+        
+        /* These are mostly from xkcd #1288. Mostly. */
+        
+        v = v.replace(/\bWitnesses\b/g, "These dudes I know");
+        v = v.replace(/\bwitnesses\b/g, "these dudes I know");
+        v = v.replace(/\bAllegedly\b/g, "Kinda probably");
+        v = v.replace(/\ballegedly\b/g, "kinda probably");
+        v = v.replace(/\bnew study\b/g, "tumblr post");
+        v = v.replace(/\bNew Study\b/g, "Tumblr Post");
+        v = v.replace(/\bRebuild\b/g, "Avenge");
+        v = v.replace(/\brebuild\b/g, "avenge");
+        v = v.replace(/\bSpace\b/g, "Spaaaaaaaace");
+        v = v.replace(/\bspace\b/g, "spaaaaaaaace");
+        v = v.replace(/\bGoogle Glass\b/g, "Virtual Boy");
+        v = v.replace(/\bSmartphone\b/g, "Pokedex");
+        v = v.replace(/\bsmartphone\b/g, "Pokedex");
+        v = v.replace(/\bElectric\b/g, "Atomic");
+        v = v.replace(/\belectric\b/g, "atomic");
+        v = v.replace(/\bSenator\b/g, "Elf-Lord");
+        v = v.replace(/\bsenator\b/g, "elf-lord");
+        v = v.replace(/\bCat\b/g, "Dinosaur");
+        v = v.replace(/\bcat\b/g, "dinosaur");
+        v = v.replace(/\bElection\b/g, "Eating Contest");
+        v = v.replace(/\belection\b/g, "eating contest");
+        v = v.replace(/\bcould not be reached for comment\b/g, "is guilty and everyone knows it");
+        v = v.replace(/\bTSA Agent\b/g, "Scrotum Fondler");
+        v = v.replace(/\bTSA agent\b/g, "scrotum fondler");
+        v = v.replace(/\bSocial justice blogger\b/g, "Pompous Ass");
+        v = v.replace(/\bsocial justice blogger\b/g, "pompous ass");
+        v = v.replace(/\bKieth Alexander\b/g, "Zod");
+        v = v.replace(/\bthe NSA\b/g, "Scooby-Doo");
+        v = v.replace(/\bThe NSA\b/g, "Scooby-Doo");
 
         textNode.nodeValue = v;
     }
